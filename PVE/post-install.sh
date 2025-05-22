2554c90635ca945e90d0a2b2da896983003b6052
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-22 12:47:03 +0200

    Update post-install.sh

--- PVE/post-install.sh
+++ PVE/post-install.sh
@@ -1,3 +1,6 @@
+# POST-INSTALL-DEBIAN
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/debian.sh)"
+# ZFS-SETTINGS
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/PVE/TOOLS/zfs-settings.sh)"
+# POST-INSTALL-PVE
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/pve/post-pve-install.sh)"
