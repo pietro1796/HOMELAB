066ae16cd7afbda1f493360778ac60c18b12cab3
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-22 12:50:37 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,10 +1,6 @@
-## PRIME CONFIGURAZIONI
-
 # Aggiornare server
 apt update & apt upgrade -y
-
 # Installare pacchetti base
 apt install sudo wget curl net-tools unzip -y
-
-## Settare TimeZone corretto      
+# Settare TimeZone corretto      
 timedatectl set-timezone Europe/Rome
