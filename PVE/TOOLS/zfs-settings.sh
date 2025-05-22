bc82f655c9a3ba09cc155b3bcda127d01c8e4d0e
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-22 12:50:07 +0200

    Update zfs-settings.sh

--- PVE/TOOLS/zfs-settings.sh
+++ PVE/TOOLS/zfs-settings.sh
@@ -1,2 +1,5 @@
+# https://pve.proxmox.com/wiki/ZFS_on_Linux#sysadmin_zfs_limit_memory_usage
+# SET MAX ARC-SIZE THIS BOOT
 echo "$[4 * 1024*1024*1024]" >/sys/module/zfs/parameters/zfs_arc_max
-options zfs zfs_arc_max=4294967296
+# SET MAX ARC-SIZE FOREVER
+sed -i '/^\s*options\s\+zfs\s\+zfs_arc_max=/c\options zfs zfs_arc_max=4294967296' /etc/modprobe.d/zfs.conf || echo 'options zfs zfs_arc_max=4294967296' | tee -a /etc/modprobe.d/zfs.conf
