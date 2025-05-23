5f4673cec5944f3bfadb68e871ac6cf5545a539b
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-23 10:02:00 +0200

    Update post-install.sh

--- PVE/post-install.sh
+++ PVE/post-install.sh
@@ -4,3 +4,5 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/Proxmo
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/debian.sh)"
 # ZFS-SETTINGS
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/PVE/TOOLS/zfs-settings.sh)"
+# GPU-SETTINGS
+bash -c "$(https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/PVE/TOOL/gpu-drivers.sh)"
