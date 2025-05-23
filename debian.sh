707630b13e6260169e92f8c91f9cec087979d87b
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-23 09:38:31 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,8 +1,12 @@
 # Aggiornare server
 apt update & apt upgrade -y
+echo "Finiti Aggiornamenti"
 # Installare pacchetti base
 apt install sudo wget curl net-tools unzip -y
+echo "Installati pacchetti"
 # Settare TimeZone corretto      
 timedatectl set-timezone Europe/Rome
+echo "Settato TimeZone"
 # Set Swappiness
 echo "vm.swappiness = 10" >> /etc/sysctl.conf
+echo "Settata Swappiness"
