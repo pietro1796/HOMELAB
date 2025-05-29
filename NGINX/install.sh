a4af1acb0b39c67a34466c422ecf263b9bbd2fee
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-05-29 22:34:09 +0200

    Update install.sh

--- NGINX/install.sh
+++ NGINX/install.sh
@@ -1,8 +1,22 @@
 apt install nginx certbot python3-certbot-nginx
 
-touch /etc/nginx/ssl/cloudflare.ini
-echo "dns_cloudflare_api_token = sguj4KHrOUCz9tjrZfVr3XfryTQPZP2rrKn8pDKu"
+nano /etc/nginx/ssl/cloudflare.ini
+
+--
+tunnel: a7ce74cf-d70a-48ab-a65f-bbeb7c52a0eb
+credentials-file: /root/.cloudflared/a7ce74cf-d70a-48ab-a65f-bbeb7c52a0eb.json
+  #ACCESSO
+ingress:
+  - hostname: "*.pietrohome.it"
+    service: https://127.0.0.1:443
+    originServerName: pietrohome.it
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+--
+
 chmod 600 /etc/nginx/ssl/cloudflare.ini
+cloudflared service install
 
 certbot certonly  --dns-cloudflare --dns-cloudflare-credentials /etc/nginx/ssl/cloudflare.ini -d '*.pietrohome.it' -d pietrohome.it --agree-tos --no-eff-email --email p.gano96@gmail.com
 ln -s /etc/letsencrypt/live/pietrohome.it/fullchain.pem /etc/nginx/ssl/pietrohome.pem
