09939f5c3ab94ea9f9100c090e749aa6d42d9159
Pietro Ganetti <p.gano96@gmail.com>
2025-10-17 10:51:18 +0200

    add max_map_count

--- PVE/post-install.sh
+++ PVE/post-install.sh
@@ -2,6 +2,8 @@
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/pve/post-pve-install.sh)"
 # POST-INSTALL-DEBIAN
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/debian.sh)"
+# Set Max Map Count
+echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/vm.max_map_count.conf
 # ZFS-SETTINGS
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/pietro1796/HOMELAB/refs/heads/main/PVE/TOOLS/zfs-settings.sh)"
 # GPU-SETTINGS
