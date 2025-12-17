658af32220be9ff792674507df14b7ce83b63259
Pietro Ganetti <77172203+pietro1796@users.noreply.github.com>
2025-12-17 12:52:06 +0100

    Enhance update script for Portainer
    Added commands to update Portainer using Docker Compose.

--- APPUNTI/DOCKER/update_portainer.sh
+++ APPUNTI/DOCKER/update_portainer.sh
@@ -3,3 +3,9 @@ docker stop portainer
 docker rm portainer
 docker pull portainer/portainer-ce:lts
 docker run -d -p 9000:9000 -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+
+
+
+## NEW VERSION ##
+cd /home/portainer
+docker compose pull && docker compose up -d
